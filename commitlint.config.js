47af8421b6a79459a2885935e0b92c567f1a8bb8
Andre Sutherland <andrelsf95@gmail.com>
2024-06-12 16:43:15 +0100

    chore: add pre-commit hook tooling

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "deploy",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+        "wip",
+      ],
+    ],
+    "scope-enum": [2, "always", []],
+  },
+};
